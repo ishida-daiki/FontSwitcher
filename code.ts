f095b87b81deedb84499ecc43cb8d06ee7cb2186
ishida-daiki <ishida-daiki@dmm.com>
2024-02-22 19:11:00 +0900

    sampleの設定を削除

--- code.ts
+++ code.ts
@@ -165,7 +165,7 @@ figma.ui.onmessage = async (msg) => {
   } else if (msg.type === "delete-style") {
     deleteDataFromClientStorage(msg.env);
   } else if (msg.type === "resize-ui") {
-    figma.ui.resize(msg.width, msg.height);
+    figma.ui.resize(msg.height);
   }
 };
 
